9f82f5bc097ba410fa033079fd76226a62dee820
Israel Canessa <israellcanessa@gmail.com>
2020-07-09 00:38:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
   array.each {|index| puts "Hello #{index}!"}
@@ -6,6 +8,6 @@ end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
-  #
+
   # Print a numbered list of each element
 end
